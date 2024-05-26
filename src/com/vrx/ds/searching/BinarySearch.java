e56af599efca98e8bde1f04f4f6442b9832fceb0
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2024-05-26 19:23:05 +0530

    binary search

---
+++ src/com/vrx/ds/searching/BinarySearch.java
@@ -0,0 +1,40 @@
+package com.vrx.ds.searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class BinarySearch {
+	/*
+	 * - Used to find element in a sorted array 1. First find out the mid point 2.
+	 * Compare that indexed value with given number a. arr[mid]==x // return mid b.
+	 * arr[mid] > x // change high to mid-1 c. arr[mid] < x // change low to mid+1
+	 * 
+	 * 
+	 */
+
+	// Return index of number if found else return -1
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the Number:");
+		int num = Integer.parseInt(reader.readLine());
+		System.out.println(bSearch(arr, num));
+	}
+
+	public static int bSearch(int[] arr, int x) {
+		int low = 0, high = arr.length - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (arr[mid] == x) {
+				return mid;
+			} else if (arr[mid] > x) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return -1;
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module dataStructures {
+}
